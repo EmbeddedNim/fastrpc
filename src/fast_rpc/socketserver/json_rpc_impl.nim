5d5a5535dc441cc7cf4e06ef8f79f15c2aadd618
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2021-12-15 19:58:55 -0800

    porting json rpc over

--- src/fast_rpc/socketserver/json_rpc_impl.nim
+++ src/fast_rpc/socketserver/json_rpc_impl.nim
@@ -1,69 +1,58 @@
-import net
-# import selectors
-# import tables
-# import posix
-
-import nephyr/logs
-import nephyr/net/tcpsocket
-
-import json
+import sets
 
 import mcu_utils/logging
 import ../inet_types
 import ../routers/router_json
 
-proc rpcJsonWriteHandler*(srv: SocketServerInfo[RpcRouter], result: ReadyKey, sourceClient: Socket, rt: RpcRouter) =
-  raise newException(OSError, "the request to the OS failed")
-
-proc rpcJsonReadHandler*(srv: TcpServerInfo[RpcRouter], result: ReadyKey, sourceClient: Socket, rt: RpcRouter) =
-
-  try:
-
-    var rcall: JsonNode
-
-    block rxblock:
-      logInfo("rpc server handler: router: %x", rt.buffer)
-
-      let rx = sourceClient.recv(rt.buffer, -1)
-      let msg = rx & "\n"
-      logInfo("rpc recv done")
-
-      if msg.len() == 0:
-        logInfo("msg too short")
-        raise newException(TcpClientDisconnected, "")
-      else:
-        echo("parse json: msgl: %d", msg.len())
-        # echo "json:msg: ", msg
-        # echo "json:msgdone "
-        rcall = parseJson(msg)
-        echo("json:done:parsed: ")
-
-    block parseblock:
-      echo("json:parsed: ")
-      for (key, item) in rcall.pairs():
-        echo "key: %s", key
-        echo "item: %s", repr(key)
-        echo " "
-      echo("json:items:done: ")
-      var res: JsonNode = rt.route( rcall )
-      var rmsg: string = $res
-
-      echo("sending to client: ", $sourceClient.getFd().int)
-      discard sourceClient.send(addr rmsg[0], rmsg.len)
-
-  except TimeoutError:
-    echo("control server: error: socket timeout: ", $sourceClient.getFd().int)
-
-proc startRpcSocketServer*(port: Port; router: var RpcRouter) =
-  logInfo("starting json rpc server: buffer: %d", router.buffer)
-
-  startSocketServer[RpcRouter](
-    port,
-    readHandler=rpcMsgPackReadHandler,
-    writeHandler=rpcMsgPackWriteHandler,
-    data=router)
+import hashes
+import json
 
 
+type 
+  JsonRpcOpts = ref object
+    router: RpcRouter
+    bufferSize: int
 
-when isMainModule:
-    runTcpServer()
+proc jsonHandler*(rt: RpcRouter, msg: var string): string =
+  var rcall: JsonNode = parseJson(msg)
+  var res: JsonNode = rt.route( rcall )
+  result = $res
+
+proc jsonRpcHandler*(srv: SocketServerInfo[JsonRpcOpts],
+                         result: ReadyKey,
+                         sourceClient: Socket,
+                         sourceType: SockType,
+                         data: JsonRpcOpts) =
+  logDebug("echoReadHandler:", "sourceClient:", sourceClient.getFd().int, "socktype:", sourceType)
+  var
+    message = newString(data.bufferSize)
+
+  case sourceType:
+  of SockType.SOCK_STREAM:
+    discard sourceClient.recv(message, data.bufferSize)
+    if message == "":
+      raise newException(InetClientDisconnected, "")
+    logDebug("received from client:", message)
+
+    var response = jsonHandler(data.router, message)
+    sourceClient.send(response)
+
+  of SockType.SOCK_DGRAM:
+    var
+      host: IpAddress
+      port: Port
+
+    discard sourceClient.recvFrom(message, message.len(), host, port)
+    var response = jsonHandler(data.router, message)
+    sourceClient.sendTo(host, port, response)
+
+  else:
+    raise newException(ValueError, "unhandled socket type: " & $sourceType)
+
+proc newJsonRpcServer*(router: RpcRouter, bufferSize = 1400): SocketServerImpl[JsonRpcOpts] =
+  new(result)
+  result.readHandler = jsonRpcHandler
+  result.writeHandler = nil 
+  result.data = new(JsonRpcOpts) 
+  result.data.bufferSize = bufferSize 
+  result.data.router = router
