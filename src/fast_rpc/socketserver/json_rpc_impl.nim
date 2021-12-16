8b3d7499e21cfcc8f44171358e5160905c73998e
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2021-12-15 23:05:30 -0800

    make json version

--- src/fast_rpc/socketserver/json_rpc_impl.nim
+++ src/fast_rpc/socketserver/json_rpc_impl.nim
@@ -1,26 +1,34 @@
 import sets
 import json
+import msgpack4nim/msgpack2json
 
 import mcu_utils/logging
 import ../inet_types
 import ../routers/router_json
 
-include common_handlers
+import common_handlers
+
+export router_json
 
 type 
   JsonRpcOpts* = ref object
     router*: RpcRouter
     bufferSize*: int
+    prefixMsgSize*: bool
 
-proc jsonRpc*(rt: RpcRouter, msg: var string): string =
-  var rcall: JsonNode = parseJson(msg)
+proc jsonRpcExec*(rt: RpcRouter, msg: var string): string =
+  logDebug("msgpack processing")
+  var rcall = msgpack2json.toJsonNode(msg)
   var res: JsonNode = rt.route( rcall )
-  result = $res
+  result = res.fromJsonNode()
 
-proc newJsonRpcServer*(router: RpcRouter, bufferSize = 1400): SocketServerImpl[JsonRpcOpts] =
+customPacketRpcHandler(packetRpcHandler, jsonRpcExec)
+
+proc newJsonRpcServer*(router: RpcRouter, bufferSize = 1400, prefixMsgSize = false): SocketServerImpl[JsonRpcOpts] =
   new(result)
-  result.readHandler = packetRpcHandler[JsonRpcOpts]
+  result.readHandler = packetRpcHandler
   result.writeHandler = nil 
   result.data = new(JsonRpcOpts) 
   result.data.bufferSize = bufferSize 
   result.data.router = router
+  result.data.prefixMsgSize = prefixMsgSize
---
+++ tests/integration/rpcjsonserver.nim
@@ -0,0 +1,32 @@
+import fast_rpc/socketserver
+import fast_rpc/socketserver/json_rpc_impl
+
+import std/monotimes
+
+const
+  VERSION = "1.0.0"
+
+# Define RPC Server #
+proc rpc_server*(): RpcRouter =
+  var rt = createRpcRouter()
+
+  rpc(rt, "version") do() -> string:
+    result = VERSION
+
+  rpc(rt, "micros") do() -> int:
+    let a = getMonoTime().ticks()
+    result = int(a div 1000)
+
+  rpc(rt, "add") do(a: int, b: int) -> int:
+    result = a + b
+
+  return rt
+
+when isMainModule:
+  let inetAddrs = [
+    newInetAddr("0.0.0.0", 5555, Protocol.IPPROTO_UDP),
+    newInetAddr("0.0.0.0", 5555, Protocol.IPPROTO_TCP),
+  ]
+
+  let router = rpc_server()
+  startSocketServer(inetAddrs, newJsonRpcServer(router, prefixMsgSize=true))
